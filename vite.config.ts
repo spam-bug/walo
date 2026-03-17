213d31168457d5c4f5957853296e6e51692b86fd
Raymark Pena <dev.mlimp@gmail.com>
2026-03-17 19:41:54 +0800

    chore: fix scaffold issues and upgrade to TailwindCSS v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     legacy()
   ],
